362189563cd11344a232b90b2012deb4e663dfd3
MONSTER <MONSTER@DESKTOP-PJVKU80>
2021-05-30 22:55:27 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı girin :");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+        }
+    }
+}
